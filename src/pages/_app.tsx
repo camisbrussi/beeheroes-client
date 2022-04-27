dea033779efe964d02b9af80be5a58ad47f21c86
camisbrussi <camila.sbrussi@universo.univates.br>
2022-04-27 20:44:35 -0300

    feat: add hotjar and google analytcs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,10 @@ import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 import { AuthProvider } from "../context/AuthContext";
+import { hotjar } from "react-hotjar";
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag";
+import { useRouter } from "next/router";
 
 import "../styles/slide.scss";
 
@@ -13,6 +17,20 @@ import "swiper/scss/scrollbar";
 import { Header } from "../components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    hotjar.initialize(0o123456, 1);
+  }, []);
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <AuthProvider>
       <ChakraProvider theme={theme}>
