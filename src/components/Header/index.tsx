076488ce3fca941cc7392b83a3ecb97cb603a4f0
camisbrussi <camila.sbrussi@universo.univates.br>
2022-05-02 15:38:16 -0300

    feat: add alert

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,11 @@
-import { Flex, useBreakpointValue, IconButton, Icon } from "@chakra-ui/react";
+import {
+  Flex,
+  useBreakpointValue,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from "@chakra-ui/react";
 import { Profile } from "./Profile";
 import { Logo } from "./Logo";
 import { Sidebar } from "../Sidebar";
@@ -9,24 +16,31 @@ export function Header() {
     lg: true,
   });
   return (
-    <Flex
-      as="header"
-      w={isWideVersion ? "100%" : "150%"}
-      //maxWidth={1480}
-      h={isWideVersion ? "65" : "30"}
-      //mx="auto"
-      align="center"
-      px="20"
-      justify="space-between"
-      borderBottom="1px"
-      borderColor="blue.600"
-      bg="blue.100"
-    >
-      <Logo />
-      <Flex>
-        <Profile />
-        <Sidebar />
+    <>
+      <Flex
+        as="header"
+        w={isWideVersion ? "100%" : "150%"}
+        //maxWidth={1480}
+        h={isWideVersion ? "65" : "30"}
+        //mx="auto"
+        align="center"
+        px="20"
+        justify="space-between"
+        borderBottom="1px"
+        borderColor="blue.600"
+        bg="blue.100"
+      >
+        <Logo />
+        <Flex>
+          <Profile />
+          <Sidebar />
+        </Flex>
       </Flex>
-    </Flex>
+      <Alert status="warning">
+        <AlertIcon />
+        Esse site foi construído para uma pesquisa do trabalho de conclusão, os
+        dados apresentados são fictícios.
+      </Alert>
+    </>
   );
 }
